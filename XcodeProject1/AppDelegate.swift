9f46b2f621f3ed9a0dd03521fdfd15401c352f06
Bryan Dossey <bryandossey@bryans-macbook-pro.local>
2017-03-21 15:55:34 -0500

    Initial Commit

---
+++ XcodeProject1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XcodeProject1
+//
+//  Created by Bryan Dossey on 3/21/17.
+//  Copyright © 2017 Bryan Dossey. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XcodeProject1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XcodeProject1
+//
+//  Created by Bryan Dossey on 3/21/17.
+//  Copyright © 2017 Bryan Dossey. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
